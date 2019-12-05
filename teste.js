35e207027d59144ba4176ddfc7be01ee6bd36f60
rafasnarf <snarfelcv@gmail.com>
2019-12-05 01:12:51 -0300

    First Commit

---
+++ teste.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $.get(
+    'https://www.googleapis.com/youtube/v3/search', {
+    key: 'AIzaSyAGgPfgloMNl6id3w9uv3O8EsAgArp_7k0',
+    part: 'snippet',
+    q: 'mussinha',
+    type: 'video',
+  },
+    function (data) {
+      var output;
+      $.each(data.items, function (i, items) {
+        console.log(items)
+        vid = items.id.videoId
+        vidTitles = items.snippet.title
+        console.log(vid)
+        console.log(vidTitles)
+        //output = '<li>' + vidTitles + '</li>';
+        output = '<li><iframe src=\"//www.youtube.com/embed/' + vid + '\"></iframe></li>'
+
+        $('#results').append(output);
+      })
+    }
+  );
+});
